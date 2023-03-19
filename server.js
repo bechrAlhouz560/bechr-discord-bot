d11e763af020626a2f8bd05c1212be69dc433b01
bechr Alhouz <82048296+bechrAlhouz560@users.noreply.github.com>
2023-03-19 14:31:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const {
-    initBot
-} = require('./bot');
-const {
-    initDb
-} = require('./db/initDb');
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const { initBot } = require("./bot");
+const { initDb } = require("./db/initDb");
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.get("/salim", function (req, res) {
+  res.send("this is salim");
+});
+app.get("", function (req, res) {
+  res.send("Welcome !");
+});
 
-app.get('/salim', function (req, res) {
-    res.send('this is salim')
-})
-app.get('', function (req, res) {
-    res.send('Welcome !')
-})
+// initDb().then(function () {
+//     app.listen(PORT, async function () {
+//         console.log('server is listening on localhost:', PORT);
+//         initBot();
+//     })
+// })
 
+initDb();
 
-
-initDb().then(function () {
-    app.listen(PORT, async function () {
-        console.log('server is listening on localhost:', PORT);
-        initBot();
-    })
-})
+module.exports = app;
